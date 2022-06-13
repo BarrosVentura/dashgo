e7f9a8f32231437f6bacf9bc407c9e22a4cf994e
Adson Ventura <adsonventura@gmail.com>
2022-06-13 14:45:14 -0300

    add: home login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,59 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="6">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
